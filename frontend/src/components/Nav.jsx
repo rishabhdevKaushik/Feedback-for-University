6930570141e12aac2551bf4844d35eff2570015d
rishabhdevKaushik <rishabhdevkaushik87@gmail.com>
2025-12-18 10:41:13 +0530

    Add logout button

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -16,7 +16,7 @@ function Nav({ toggleMenu, setToggleMenu }) {
 
   return (
     <>
-      <nav className='absolute  gap-[0.625rem] md:relative md:mx-10 md:mb-10 md:flex md:h-[11.125rem] xl:mr-0 xl:flex xl:flex-col xl:gap-6 xl:pr-0 xl:pt-24'>
+      <nav className='absolute hidden gap-[0.625rem] md:relative md:mx-10 md:mb-10 md:flex md:h-[11.125rem] xl:mr-0 xl:flex xl:flex-col xl:gap-6 xl:pr-0 xl:pt-24'>
         <div className='h-[11.125rem] w-1/3 rounded-xl bg-gradient-to-tr from-gradient-blue via-gradient-purple to-gradient-red px-6 pt-24 lg:mr-0 lg:w-1/3 xl:w-full xl:min-w-64 xl:p-6 xl:pt-16'>
           <p className='text-xl font-bold tracking-[-0.25px] text-white'>
             Feedback and Suggestion Box
@@ -28,19 +28,17 @@ function Nav({ toggleMenu, setToggleMenu }) {
         <Filter />
         <Roadmap />
         {isAdmin && <AdminDashboard />}
-        {/* {isAdmin && (
-          <div className='rounded-xl bg-white p-6 xs:mx-3 s:mx-6 md:m-0 md:w-1/3 md:p-6 lg:mx-2 lg:w-1/3 xl:m-0 xl:h-16 xl:w-full xl:min-w-64'>
-            <button
-              onClick={() => {
-                localStorage.clear();
-                window.location.href = '/';
-              }}
-              className='w-full rounded-lg bg-red-500 px-4 py-2 text-center text-white font-semibold hover:bg-red-600 transition-colors'
-            >
-              Logout (Admin)
-            </button>
-          </div>
-        )} */}
+        <div className='rounded-xl bg-white p-6 xs:mx-3 s:mx-6 md:m-0 md:w-1/3 md:p-6 lg:mx-2 lg:w-1/3 xl:m-0 xl:h-16 xl:w-full xl:min-w-64 flex items-center justify-center'>
+          <button
+            onClick={() => {
+              localStorage.clear();
+              window.location.href = '/';
+            }}
+            className='w-full rounded-lg bg-red px-4 py-2 text-center text-white font-semibold hover:bg-strong-red transition-colors'
+          >
+            Logout
+          </button>
+        </div>
       </nav>
       <nav className='flex h-[4.5rem] items-center justify-between overflow-x-hidden bg-gradient-to-tr from-gradient-blue via-gradient-purple to-gradient-red xs:px-4 s:px-6 md:absolute md:hidden'>
         <div className='flex flex-col text-white'>
